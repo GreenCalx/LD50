ea25d736eed00bbeceff9b2d0e74a62ccc578fd6
Etienne Lomet <tino.tablette@gmail.com>
2022-04-02 18:14:03 +0200

    gestion camera UI

--- LD50/Assets/Scripts/GestionUI.cs
+++ LD50/Assets/Scripts/GestionUI.cs
@@ -2,12 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class GestionUI : MonoBehaviour
 {
+    public GameObject modules_group;
+    private Module[] modules;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        modules = modules_group.GetComponentsInChildren<Module>();
     }
 
     // Update is called once per frame
