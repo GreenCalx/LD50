ed37c6785bd2c60994551ac184ca0328d171aa0b
GreenCalx <romain.schoenberger@gmail.com>
2022-04-04 18:49:29 +0200

    sawer enemy

---
+++ LD50/Assets/Scripts/Sawer.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sawer : Enemy
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        BaseStart();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        BaseUpdate();
+    }
+}
